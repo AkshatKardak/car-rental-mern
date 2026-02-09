7eabdee93cfc2c3291c221b0cc4c87a23bcd0042
AkshatKardak Github <kardakakshat@gmail.com>
2026-02-09 23:18:30 +0530

    Fix: Remove wildcard options route for Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,7 +34,6 @@ connectDB();
 const authRoutes = require('./routes/authRoutes');
 const carRoutes = require('./routes/carRoutes');
 const bookingRoutes = require('./routes/bookingRoutes');
-const userRoutes = require('./routes/userRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
 
 // API Routes
